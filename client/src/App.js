9f2e2816c8cfc74c172a80eb9e648d9c44a40d0f
rtolefree1 <r_ryde2000@yahoo.com>
2023-06-27 19:18:24 -0400

    adding skeleton to github

---
+++ client/src/App.js
@@ -0,0 +1,37 @@
+// import logo from './logo.svg';
+// import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import about from './pages/about';
+import CustomerLogin from './pages/loginCustomer';
+import GuestLogin from './pages/loginGuest';
+import NotFound from './pages/NotFound';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
