9205fdc26ed75e4d919c47d6510a3da7735b43b0
Kaitlyn Atif <kaitlynatif90@hotmail.com>
2023-07-03 04:05:26 -0400

    Added LogIn Form. Fixed Navigation bar location.

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,6 +1,15 @@
-import React from 'react';
-const Footer =() =>{
-    return <div></div>
-    }
+import React from "react";
+const Footer = () => {
+  return (
+    <div>
+      <footer class="container">
+        <p class="float-end">
+          <a href="./client/public/index.html">Back to top</a>
+        </p>
+        <p>&copy; 2023 Salt Your Food Reservation App, Inc.</p>
+      </footer>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
